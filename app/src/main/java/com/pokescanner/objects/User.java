38554cf4d6b70d4e39c8a01d20c3a17e9d276458
Toms Kusiņš <toms@fullcontact.com>
2016-07-23 04:57:42 +0300

    Update Gradle, shorten data classes

--- app/src/main/java/com/pokescanner/objects/User.java
+++ app/src/main/java/com/pokescanner/objects/User.java
@@ -18,10 +18,17 @@ package com.pokescanner.objects;
 
 import io.realm.RealmObject;
 import io.realm.annotations.PrimaryKey;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 
 /**
  * Created by Brian on 7/22/2016.
  */
+@Data
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
 public class User extends RealmObject{
     static public final int PTC = 0;
     static public final int GOOGLE = 1;
@@ -32,55 +39,5 @@ public class User extends RealmObject{
     String password;
     int authType;
 
-    public User(int index,String username, String password, int authType) {
-        this.index = index;
-        this.username = username;
-        this.password = password;
-        this.authType = authType;
-    }
-
-    public User() {
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public int getIndex() {
-        return index;
-    }
-
-    public void setIndex(int index) {
-        this.index = index;
-    }
-
-    public int getAuthType() {
-        return authType;
-    }
-
-    public void setAuthType(int authType) {
-        this.authType = authType;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "index=" + index +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", authType=" + authType +
-                '}';
-    }
+    public User() {}
 }
